c966a5f8bc99f9c96db375f3dc255a136a6266d9
Jona <jonalexba@gmail.com>
2023-02-27 19:02:34 -0300

    +mi repo

---
+++ FUNDAMENTOS-PROG/tipo-de-variable.js
@@ -0,0 +1,4 @@
+let numero;      // Defino la variable x
+numero = 5;      // Le asigno un valor numérico
+numero = "Jon";  // Ahora le asigno una cadena
+console.log(numero);
--- FUNDAMENTOS-PROG/variable.js
+++ FUNDAMENTOS-PROG/variable.js
@@ -7,3 +7,27 @@ console.log(nombre);
 nombre = 33;
 
 console.log(nombre);
+
+//examples
+
+// Numbers:
+let length = 16;
+let weight = 7.5;
+
+// Strings:
+let color = "Yellow";
+let lastName = "Johnson";
+
+// Booleans
+let x = true;
+let y = false;
+
+// Object:
+const person = {firstName:"John", lastName:"Doe"};
+let xy = {firstName:"John", lastName:"Doe"};
+
+// Array object:
+const cars = ["Saab", "Volvo", "BMW"];
+
+// Date object:
+const date = new Date("2022-03-25");

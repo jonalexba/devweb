18ce35815ec7bca9747c526fa6921b789bd00844
Jona <jonalexba@gmail.com>
2023-03-08 15:46:37 -0300

    Proyecto Final

---
+++ FUNDAMENTOS-PROG/CV/funciones.js
@@ -0,0 +1,5 @@
+document.getElementById("botonMostrar").addEventListener('click', function() {
+   
+    document.getElementById('contactoDiv').style.display = '';
+    console.log("muestra el texto");
+});
